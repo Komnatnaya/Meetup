6f38527d05d40e414ce843bbea7a20afb9ab5923
Katerina Komnatnaya <comina.ru@gmail.com>
2017-05-22 21:00:28 +0300

    map+js

--- js/script.js
+++ js/script.js
@@ -1,36 +1,54 @@
 'use strict';
-// Таймер
+// Timer
 	function startTimer() {
 	var timer = document.getElementById("timer");
 	var time = timer.innerHTML;
 	var arr = time.split(":");
-	var d = arr[0];
-	var h = arr[1];
-	var m = arr[2];
-	var s = arr[3];
-	if (s == 0) {
-		if (m == 0) {
-			if (h == 0) {
-				if (d == 0) {
+	var day = arr[0];
+	var hour = arr[1];
+	var minute = arr[2];
+	var second = arr[3];
+	if (second == 0) {
+		if (minute == 0) {
+			if (hour == 0) {
+				if (day == 0) {
 					window.location.reload();
 					return;
 				}
-				d--;
-				h = 24;
-				if (h < 10) h = "0" + d;
+				day--;
+				hour = 24;
+				if (hour < 10) hour = "0" + day;
 			}
-			h--;
-			m = 60;
-			if (h < 10) h = "0" + h;
+			hour--;
+			minute = 60;
+			if (hour < 10) hour = "0" + hour;
 		}
-		m--;
-		if (m < 10) m = "0" + m;
-		s = 59;
+		minute--;
+		if (minute < 10) minute = "0" + minute;
+		second = 59;
 	}
-	else s--;
-	if (s < 10) s = "0" + s;
-	document.getElementById("timer").innerHTML = d+" : "+h+" : "+m+" : "+s;
+	else second--;
+	if (second < 10) second = "0" + second;
+	document.getElementById("timer").innerHTML = day+" : "+hour+" : "+minute+" : "+second;
 	setTimeout(startTimer, 1000);
 }
 
 startTimer();
+
+
+// Map
+
+var popup = document.querySelector(".contact__overlay-map");
+var viewMap = popup.querySelector(".contact__view-map");
+var contactShow = document.querySelector(".contact__show");
+
+viewMap.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("contact__overlay-map--none");
+	contactShow.classList.add("contact__show--show");
+});
+contactShow.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("contact__overlay-map--none");
+	contactShow.classList.remove("contact__show--show");
+});
